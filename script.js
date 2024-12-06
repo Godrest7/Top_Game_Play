5142cf1a6181d4192b5d6911ce84df184be26328
Godrest7 <ngodwineveresth007@gmail.com>
2024-12-06 02:44:40 +0100

    Ajout des jeux

---
+++ script.js
@@ -0,0 +1,44 @@
+const gamesList = [
+	{
+		title: "Detroit: Become Human",
+		year: 2018,
+		imageUrl:
+			"https://wallpaperaccess.com/full/1489919.jpg",
+		id: 1,
+	},
+	{
+		title: "Resident Evil Village",
+		year: 2021,
+		imageUrl:
+			"https://cdn.wccftech.com/wp-content/uploads/2021/01/WCCFresidentevilvillage8.jpg",
+		id: 2,
+	},
+	{
+		title: "EA SPORTS FC™ 24",
+		year: 2023,
+		imageUrl:
+			"https://www.fifa-infinity.com/wp-content/uploads/2024/01/fc24-vinicius-real-madrid.webp",
+		id: 3,
+	},
+	{
+		title: "Street Fighter V",
+		year: 2015,
+		imageUrl:
+			"https://gaming-cdn.com/images/products/671/orig/street-fighter-v-pc-jeu-steam-cover.jpg?v=1662539255",
+		id: 4,
+	},
+	{
+		title: "The Last of Us",
+		year: 2013,
+		imageUrl:
+			"https://www.4gamer.net/games/636/G063663/20220830067/SS/032.jpg",
+		id: 5,
+	},
+	{
+		title: "Horizon Zero Dawn",
+		year: 2017,
+		imageUrl:
+			"https://wallpapercave.com/wp/wp6601789.jpg",
+		id: 6,
+	},
+]
