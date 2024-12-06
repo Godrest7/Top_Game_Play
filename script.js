69a3c1bb8b0a5f2975e70b9ec30334c481b97755
Godrest7 <ngodwineveresth007@gmail.com>
2024-12-06 03:29:57 +0100

    ajout de <img src="${game.imageUrl}" alt="${game.title}" class="card-img-top" />

--- script.js
+++ script.js
@@ -81,22 +81,7 @@ function writeDom() {
 		const articleContainer = document.querySelector(".row")
 		articleContainer.innerHTML += `<article class="col">
                         <div class="card shadow-sm">
-                            <svg
-                                class="bd-placeholder-img card-img-top"
-                                width="100%"
-                                height="225"
-                                xmlns="http://www.w3.org/2000/svg"
-                                role="img"
-                                aria-label="Placeholder: Thumbnail"
-                                preserveAspectRatio="xMidYMid slice"
-                                focusable="false"
-                            >
-                                <title>Placeholder</title>
-                                <rect width="100%" height="100%" fill="#55595c"></rect>
-                                <text x="50%" y="50%" fill="#eceeef" dy=".3em">
-                                Thumbnail
-                                </text>
-                            </svg>
+                         <img src="${game.imageUrl}" alt="${game.title}" class="card-img-top" />
                             <div class="card-body">
                             <h3 class="card-title">${game.title}</h3>
                                 <p class="card-text">${game.year}</p>
