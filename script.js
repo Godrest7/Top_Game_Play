65903985d2b651280f757f5a59e503f60b7eabdf
Godrest7 <ngodwineveresth007@gmail.com>
2024-12-06 04:04:05 +0100

    correction

--- script.js
+++ script.js
@@ -129,7 +129,7 @@ function writeDom() {
                                 <p class="card-text">${game.year}</p>
                                 <div class="d-flex justify-content-between align-items-center">
                                     <div class="btn-group">
-                                        <button type="button" class="btn btn-sm btn-outline-secondary "
+                                        <button type="button" class="btn btn-sm btn-outline-secondary view"
                                         data-bs-toggle="modal" data-bs-target="#exampleModal"
                                         >View</button>
                                         <button type="button" class="btn btn-sm btn-outline-secondary edit"
