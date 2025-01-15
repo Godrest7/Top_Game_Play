c92fac77b15d1cd3090de0a61d09ce2b2df9c5fd
ArameBeudeu <157950887+ArameBeudeu@users.noreply.github.com>
2025-01-15 15:37:39 +0100

    ajout

--- login.js
+++ login.js
@@ -10,3 +10,37 @@ form.addEventListener("submit", (e) => {
 	}
 	console.log(formdata)
 })
+
+// input validation
+
+function validateName(input) {
+	if (!input || input.trim() === "") {
+		throw new Error("L'user name est vide !")
+	}
+
+	const alphanumericRegex = /^[a-zA-Z0-9]+$/
+	if (!alphanumericRegex.test(input)) {
+		throw new Error(
+			"L'entrée doit contenir uniquement des caractères alphanumériques."
+		)
+	}
+}
+
+function validateEmail(input) {
+	if (!input || input.trim() === "") {
+		throw new Error("L'email name est vide")
+	}
+
+	const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+	if (!emailRegex.test(input)) {
+		return "L'entrée doit contenir uniquement des caractères alphanumériques."
+	}
+}
+
+function showError(error) {
+	document.querySelector(".alert").innerText = error
+	document.querySelector(".alert").classList.remove("d-none")
+	setTimeout(() => {
+		document.querySelector(".alert").classList.add("d-none")
+	}, 2100)
+}
